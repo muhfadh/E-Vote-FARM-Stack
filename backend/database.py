45f5d42b5f60e30a1a58c8b7d84752905f6074b6
Muhammad Fadhlan <oxide108fa@gmail.com>
2022-06-14 20:30:50 +0700

    add databse.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,7 @@
-from pydoc import doc
-from typing import Collection
 from model import Candidates
 
 # MongoDB Driver
-import motor.motor_asyncio 
+import motor.motor_asyncio
 
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 
@@ -21,3 +19,25 @@ async def fetch_all_candidates():
     async for document in cursor:
         candidates.append(Candidates(**document))
     return candidates
+
+async def create_candidates(candidates):
+    document = candidates
+    result = await collection.insert_one(document)
+    return document
+
+async def update_candidates(candidate_id, name, vision, mission):
+    await collection.update_one(
+        {"candidate_id": candidate_id}, 
+        {"$set": {"name": name, "vision": vision, "mission": mission}})
+
+    document = await collection.find_one({"candidate_id": candidate_id})
+
+    return document
+
+async def remove_candidates(candidate_id):
+    try:
+        await collection.delete_one({"candidate_id": candidate_id})
+    except:
+        return False
+    return True
+
